168142584d83e6a858ee804719830378661ae25a
jemag <bd.jemag@gmail.com>
2019-01-01 16:09:16 -0500

    Make polybar more dynamic with monitors, fix polybar volume display

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,10 +1,10 @@
 killall polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
-monitors=$(xrandr -q| grep -n "connected"| wc -l)
-if [ $monitors = 1 ]; then
-	polybar single &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
 else
-	polybar one &
-	polybar two &
+  polybar --reload main &
 fi
