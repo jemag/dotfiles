1c86cbb66c166a3f9f743faf7c72c4f0d374fcc0
jemag <bd.jemag@gmail.com>
2019-01-01 17:46:09 -0500

    Use polybar to retrieve monitor list in launch.sh

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,7 +2,7 @@ killall polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload main &
   done
 else
