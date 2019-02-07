93443b86f7340388b1f1668d0327adc76b967056
jemag <bd.jemag@gmail.com>
2019-02-06 20:24:37 -0500

    Add status color patch + some basic colors

--- dwm/dwm/config.h
+++ dwm/dwm/config.h
@@ -6,17 +6,16 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Fura Mono Nerd Font Complete:size=10" };
+static const char dmenufont[]       = "Fura Mono Nerd Font Complete:size=10";
+static const char col_vdark[]       = "#292d36";
+static const char col_dark[]        = "#3a404d";
+static const char col_norm[]        = "#8790a3";
+static const char col_full[]        = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_norm,  col_vdark,  col_norm },
+	[SchemeSel]  = { col_full,  col_vdark,  col_full },
 };
 
 /* tagging */
@@ -60,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_vdark, "-nf", col_norm, "-sb", col_dark, "-sf", col_full, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
