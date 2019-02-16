b1df256070caa3289b9936b28933ae1f7a95d003
jemag <bd.jemag@gmail.com>
2019-02-16 15:22:16 -0500

    Use proper skin color for dmenu

--- dwm/dwm/config.h
+++ dwm/dwm/config.h
@@ -79,7 +79,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", white, "-sb", darkcyan, "-sf", white, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", skin, "-sb", darkcyan, "-sf", white, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
