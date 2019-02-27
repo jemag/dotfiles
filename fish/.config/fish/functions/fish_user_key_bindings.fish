df287db5706209717b4cbf7ba9495421f2d21508
jemag <bd.jemag@gmail.com>
2019-02-26 19:55:47 -0500

    Add fish keybinds for forward char (instead of using right arrow)

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,5 @@
 function fish_user_key_bindings
+    bind -k \cF forward-char
+    bind -M insert \cF forward-char 
     bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
