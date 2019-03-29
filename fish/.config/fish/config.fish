a055e86ace256fa1bba75d67cdccc6a85edbcc3b
jemag <bd.jemag@gmail.com>
2019-03-28 21:01:36 -0400

    Add EDITOR and JAVA_HOME variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,5 @@ fish_vi_key_bindings
 set -g theme_display_user yes
 set -g fish_prompt_pwd_dir_length 0
 set -gx PATH /home/jemag/bin /home/jemag/.gem/ruby/2.6.0/bin $PATH
+set -Ux EDITOR vim
+set -Ux JAVA_HOME /usr/lib/jvm/default
