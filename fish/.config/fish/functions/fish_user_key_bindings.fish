07bb7856aed5dcee8c0442c1bc0bbd584428bb32
jemag <bd.jemag@gmail.com>
2019-05-14 21:07:57 -0400

    Add /home/jemag/bin to fish path + fish fzf key bindings

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -3,3 +3,5 @@ function fish_user_key_bindings
     bind -M insert \cF forward-char 
     bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
+
+fzf_key_bindings
---
+++ fish/.config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/jemag/.fzf/shell/key-bindings.fish
