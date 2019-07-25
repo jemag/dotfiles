c134ed1f46a04b287c1b0f7dc70fa15dcf5aae08
jemag <bd.jemag@gmail.com>
2019-07-25 16:45:59 -0400

    Set nvim as editor, add completion for aws cli

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,7 @@ fish_vi_key_bindings
 set -g theme_display_user yes
 set -g fish_prompt_pwd_dir_length 0
 set -gx PATH /home/jemag/bin /home/jemag/.gem/ruby/2.6.0/bin $PATH
-set -Ux EDITOR vim
+set -Ux EDITOR nvim
 set -Ux JAVA_HOME /usr/lib/jvm/default
 alias k="kubectl"
 alias tf="terraform"
