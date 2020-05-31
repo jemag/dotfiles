8183261330b4525188c5b52f488d8d1031697fa1
jemag <alexandre.bd@tutanota.com>
2020-05-31 16:49:55 -0400

    Remove alt+l from keybinds in st

--- st/st/config.h
+++ st/st/config.h
@@ -253,7 +253,6 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_U,           zoom,           {.f = +2} },
 	{ TERMMOD,              XK_D,           zoom,           {.f = -2} },
-	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
 };
