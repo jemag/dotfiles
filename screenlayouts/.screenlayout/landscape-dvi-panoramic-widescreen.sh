f082e35d733c1e85a2861ab6b31cc5f3e4a01109
jemag <alexandre.bd@tutanota.com>
2020-06-05 20:23:15 -0400

    Add new double screen home layout

---
+++ screenlayouts/.screenlayout/landscape-dvi-panoramic-widescreen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-1 --off --output DVI-D-0 --off
