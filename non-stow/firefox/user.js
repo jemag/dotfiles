029393e85d7de0136fb1115964a81f9d35f5f51f
jemag <alexandre.bd@tutanota.com>
2020-11-28 23:21:23 -0500

    try to make firefox background dark when loading new websites + tabs

--- non-stow/firefox/user.js
+++ non-stow/firefox/user.js
@@ -9,3 +9,5 @@
  ******************************************************************************/
 user_pref("browser.tabs.insertAfterCurrent", true);
 user_pref("browser.tabs.warnOnClose", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.display.background_color", "#282828");
