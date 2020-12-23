2589ffe78916293c9a78549fa7369124c4709ae0
jemag <alexandre.bd@tutanota.com>
2020-12-22 22:28:26 -0500

    fix firefox right click selecting menu item right away

--- non-stow/firefox/user.js
+++ non-stow/firefox/user.js
@@ -11,3 +11,4 @@ user_pref("browser.tabs.insertAfterCurrent", true);
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.display.background_color", "#282828");
+user_pref("ui.context_menus.after_mouseup", true);
