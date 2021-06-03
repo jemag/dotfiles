81cbdd37d730a6b585e57112c5de7e8a1d7fb3e2
Alexandre Desjardins <alexandre.bd@tutanota.com>
2021-06-03 18:41:48 -0400

    add treesitter indent + fold, disable default indent blankline

--- neovim/.config/nvim/lua/plugin-configs/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugin-configs/indent-blankline.lua
@@ -1,5 +1,5 @@
 vim.g.indent_blankline_char = '|'
 vim.g.indent_blankline_show_current_context = true
--- vim.g.indent_blankline_enabled = true
+vim.g.indent_blankline_enabled = false
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile', 'packer'}
 vim.api.nvim_set_keymap('n', '<localleader>ti','<cmd>IndentBlanklineToggle<CR>', { noremap = true, silent = true })
