62b71a547d702d02fcf90098cc472a960d201233
jemag <alexandre.bd@tutanota.com>
2021-08-06 21:51:54 -0400

    auto open nvim tree on directory

--- neovim/.config/nvim/lua/plugin-configs/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugin-configs/nvim-tree.lua
@@ -1,2 +1,5 @@
 vim.g.nvim_tree_width = 65
 vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
+vim.g.nvim_tree_hijack_netrw = 1
+vim.g.nvim_tree_auto_open = 1
+vim.g.nvim_tree_auto_ignore_ft = {'startify', 'dashboard'}
