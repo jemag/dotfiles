370b4af2d22554ce90d95ebe0080c4aefe75a4c3
Alexandre Desjardins <alexandre.bd@tutanota.com>
2021-12-03 15:44:15 -0500

    add git add -u git alias

--- neovim/.config/nvim/lua/plugin-configs/telescope-config.lua
+++ neovim/.config/nvim/lua/plugin-configs/telescope-config.lua
@@ -64,7 +64,7 @@ require('telescope').setup {
   pickers = {
     -- Your special builtin config goes in here
     buffers = {
-      sort_lastused = true,
+      sort_lastused = false,
       mappings = {
         i = {
           ["<c-s>"] = require("telescope.actions").delete_buffer,
