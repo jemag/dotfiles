137e4b0a9d2bd3815c5919a4d9608bf0a519c960
jemag <alexandre.bd@tutanota.com>
2021-12-19 13:20:06 -0500

    use stylua config with spaces instead of tabs

--- neovim/.config/nvim/lua/plugin-configs/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugin-configs/indent-blankline.lua
@@ -1,30 +1,30 @@
-vim.api.nvim_set_keymap('n', '<localleader>ti','<cmd>IndentBlanklineToggle<CR>', { noremap = true, silent = true })
-vim.cmd [[autocmd ColorScheme * highlight! IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[autocmd ColorScheme * highlight! IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[autocmd ColorScheme * highlight! IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[autocmd ColorScheme * highlight! IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[autocmd ColorScheme * highlight! IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[autocmd ColorScheme * highlight! IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+vim.api.nvim_set_keymap("n", "<localleader>ti", "<cmd>IndentBlanklineToggle<CR>", { noremap = true, silent = true })
+vim.cmd([[autocmd ColorScheme * highlight! IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+vim.cmd([[autocmd ColorScheme * highlight! IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+vim.cmd([[autocmd ColorScheme * highlight! IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+vim.cmd([[autocmd ColorScheme * highlight! IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+vim.cmd([[autocmd ColorScheme * highlight! IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+vim.cmd([[autocmd ColorScheme * highlight! IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
-require("indent_blankline").setup {
-    char = '|',
-    enabled = false,
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
-    filetype_exclude = {
-         "help",
-         "terminal",
-         "dashboard",
-         "nofile",
-         "packer",
-         "lspinfo",
-         "TelescopePrompt",
-         "TelescopeResults",
-      },
-}
+require("indent_blankline").setup({
+  char = "|",
+  enabled = false,
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+    "IndentBlanklineIndent5",
+    "IndentBlanklineIndent6",
+  },
+  filetype_exclude = {
+    "help",
+    "terminal",
+    "dashboard",
+    "nofile",
+    "packer",
+    "lspinfo",
+    "TelescopePrompt",
+    "TelescopeResults",
+  },
+})
