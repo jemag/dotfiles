ad38cf7509ece94a268c67a92135188897f3080d
jemag <alexandre.bd@tutanota.com>
2022-02-08 21:19:05 -0500

    change treesitter incremental selection keybinds

--- neovim/.config/nvim/lua/plugin-configs/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugin-configs/nvim-treesitter.lua
@@ -10,10 +10,10 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     disable = {"phpdoc"},
     keymaps = { -- mappings for incremental selection (visual mappings)
-      init_selection = "gnn", -- maps in normal mode to init the node/scope selection
-      node_incremental = "grn", -- increment to the upper named parent
+      init_selection = "<cr>", -- maps in normal mode to init the node/scope selection
+      node_incremental = "<cr>", -- increment to the upper named parent
       scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
-      node_decremental = "grm", -- decrement to the previous node
+      node_decremental = "<bs>", -- decrement to the previous node
     },
   },
   refactor = {
