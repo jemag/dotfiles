c28f3a5b7fc7967b8cc2b844fc917483c681077c
Alexandre Desjardins <alexandre.bd@tutanota.com>
2022-04-05 15:50:10 -0400

    add search count to lualine, fix barbar modified file color

--- neovim/.config/nvim/lua/plugin-configs/lualine.lua
+++ neovim/.config/nvim/lua/plugin-configs/lualine.lua
@@ -1,6 +1,16 @@
 local custom_ayu_mirage = require'lualine.themes.ayu_mirage'
 -- Change the background of lualine_c section for normal mode
 custom_ayu_mirage.inactive.c.bg = '#111111'
+
+local function search_cnt()
+  local res = vim.fn.searchcount()
+
+  if res.total > 0 then
+    return string.format("%s/%d %s", res.current, res.total, vim.fn.getreg('/'))
+   else
+     return ""
+  end
+end
 require("lualine").setup({
 	options = {
     globalstatus = true,
@@ -11,12 +21,12 @@ require("lualine").setup({
 		disabled_filetypes = {},
 	},
 	sections = {
-		lualine_a = { "mode" },
+		lualine_a = { "mode"},
 		lualine_b = { "branch" },
 		lualine_c = { "filename" },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
-		lualine_z = { "location" },
+		lualine_z = { search_cnt, "location" },
 	},
 	inactive_sections = {
 		lualine_a = {},
