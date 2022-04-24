45e6736b5b81a34f446d760962ed3cad0f97da31
jemag <alexandre.bd@tutanota.com>
2022-04-24 18:53:17 -0400

    allow only neo-tree to hijack netrw

--- neovim/.config/nvim/lua/plugin-configs/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugin-configs/nvim-tree.lua
@@ -1,6 +1,6 @@
 require("nvim-tree").setup({
 	disable_netrw = false,
-	hijack_netrw = true,
+	hijack_netrw = false,
 	open_on_setup = false,
 	ignore_ft_on_setup = { "startify", "dashboard" },
 	open_on_tab = false,
