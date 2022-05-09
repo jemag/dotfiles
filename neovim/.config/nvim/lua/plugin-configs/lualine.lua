3d62dea0aed3f0be0cd53d4e1f18cb47f34d3152
jemag <alexandre.bd@tutanota.com>
2022-05-08 23:17:42 -0400

    change lualine filename fg color

--- neovim/.config/nvim/lua/plugin-configs/lualine.lua
+++ neovim/.config/nvim/lua/plugin-configs/lualine.lua
@@ -23,7 +23,7 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode"},
 		lualine_b = { "branch" },
-		lualine_c = { {"filename", path = 1} },
+		lualine_c = { {"filename", path = 1, color = {fg = '#ffffff'}} },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { search_cnt, "location" },
