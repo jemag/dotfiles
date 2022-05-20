11f333908b02195bdee4f15c5ec75bf384c4a0d6
Alexandre Desjardins <alexandre.bd@tutanota.com>
2022-05-20 17:41:06 -0400

    change winbar text color

--- neovim/.config/nvim/lua/ui/winbar.lua
+++ neovim/.config/nvim/lua/ui/winbar.lua
@@ -1,7 +1,7 @@
 local winbar = {}
 
 vim.api.nvim_set_hl(0, "WinBarSeparator", { fg = "#1F2430" })
-vim.api.nvim_set_hl(0, "WinBarContent", { fg = "#626A73", bg = "#1F2430" })
+vim.api.nvim_set_hl(0, "WinBarContent", { fg = "#F29E74", bg = "#1F2430" })
 
 winbar.eval = function()
   if vim.api.nvim_eval_statusline("%f", {})["str"] == "[No Name]" then
