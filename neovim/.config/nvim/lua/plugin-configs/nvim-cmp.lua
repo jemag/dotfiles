5dda4f908653e25f29857bbe8fd7205715aad38f
jemag <alexandre.bd@tutanota.com>
2022-05-23 11:03:05 -0400

    improve cmp appearance

--- neovim/.config/nvim/lua/plugin-configs/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugin-configs/nvim-cmp.lua
@@ -1,13 +1,40 @@
-local lspkind = require("lspkind")
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 -- lazy load all vscode like snippets
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+  local col = vim.fn.col(".") - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
+
+local icons = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "⌘",
+    Field = "ﰠ",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "塞",
+    Value = "",
+    Enum = "",
+    Keyword = "廓",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "פּ",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
+}
 
 cmp.setup({
   preselect = cmp.PreselectMode.Item,
@@ -17,8 +44,8 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s", "c"}),
-    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s", "c"}),
+    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s", "c" }),
+    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s", "c" }),
     -- ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
@@ -70,22 +97,19 @@ cmp.setup({
     { name = "buffer" },
   },
   formatting = {
-    format = lspkind.cmp_format({
-      with_text = true,
-      menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        luasnip = "[snip]",
-      },
-    }),
+    fields = { "kind", "abbr", "menu" },
+    format = function(_, vim_item)
+      vim_item.menu = vim_item.kind
+      vim_item.kind = icons[vim_item.kind]
+
+      return vim_item
+    end,
   },
   window = {
     documentation = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
       -- winhighlight = "NormalFloat:Normal",
-    }
+    },
   },
   experimental = {
     ghost_text = true,
