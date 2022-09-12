874e339fe3b5eaaf7cafbce1df9baf2502778331
jemag <alexandre.bd@tutanota.com>
2022-09-11 22:36:32 -0400

    update nvim-surround config

--- neovim/.config/nvim/lua/plugin-configs/nvim-surround.lua
+++ neovim/.config/nvim/lua/plugin-configs/nvim-surround.lua
@@ -6,49 +6,7 @@ require("nvim-surround").setup({
     delete = "ds",
     change = "cs",
   },
-  delimiters = {
-    pairs = {
-      ["("] = { "( ", " )" },
-      [")"] = { "(", ")" },
-      ["{"] = { "{ ", " }" },
-      ["}"] = { "{", "}" },
-      ["<"] = { "< ", " >" },
-      [">"] = { "<", ">" },
-      ["["] = { "[ ", " ]" },
-      ["]"] = { "[", "]" },
-      -- Define pairs based on function evaluations!
-      ["i"] = function()
-        return {
-          require("nvim-surround.utils").get_input("Enter the left delimiter: "),
-          require("nvim-surround.utils").get_input("Enter the right delimiter: "),
-        }
-      end,
-      ["f"] = function()
-        return {
-          require("nvim-surround.utils").get_input("Enter the function name: ") .. "(",
-          ")",
-        }
-      end,
-    },
-    separators = {
-      ["'"] = { "'", "'" },
-      ['"'] = { '"', '"' },
-      ["`"] = { "`", "`" },
-    },
-    HTML = {
-      ["t"] = true, -- Use "t" for HTML-style mappings
-    },
-    aliases = {
-      ["a"] = ">", -- Single character aliases apply everywhere
-      ["b"] = ")",
-      ["B"] = "}",
-      ["r"] = "]",
-      -- Table aliases only apply for changes/deletions
-      ["q"] = { '"', "'", "`" }, -- Any quote character
-      ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
-    },
-  },
-  highlight_motion = { -- Highlight text-objects before surrounding them
+  highlight = { -- Highlight text-objects before surrounding them
     duration = 0,
   },
 })
