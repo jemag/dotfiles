b579638f88f6d2a5b8db9e9b6d69ab9d3dd40fe8
jemag <alexandre.bd@tutanota.com>
2022-09-11 22:34:00 -0400

    change neovim build from source script

--- non-stow/neovim/create-nightly-binary.sh
+++ non-stow/neovim/create-nightly-binary.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-ROOT_DIR="$HOME/Projects/github-reference/neovim"
-BUILD_DIR="$ROOT_DIR/build"
-SOURCE_DIR="$ROOT_DIR/src"
-
-# Extract the source and if there were no errors, remove the previous build.
-cd $SOURCE_DIR
-git pull &&
-	make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$BUILD_DIR &&
-	rm -rf $BUILD_DIR &&
-	make install &&
-	git reset --hard; git clean -fxd &&
-	cd -
-ln -s "$BUILD_DIR/bin/nvim" ~/bin/nvim
+set -euo pipefail
+cd /tmp || exit
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
