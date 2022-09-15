fdabbecce4e9e33c99649e1f149a5c337b505ffa
jemag <alexandre.bd@tutanota.com>
2022-09-14 22:38:06 -0400

    remove neovim folder before cloning in build script

--- non-stow/neovim/create-nightly-binary.sh
+++ non-stow/neovim/create-nightly-binary.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
+[ -d "/tmp/neovim" ] && rm -rf /tmp/neovim
 cd /tmp || exit
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
+make install
