9d0876cfeebb1e7d7ea0904f851d49c3b0c1d5c7
jemag <alexandre.bd@tutanota.com>
2022-10-08 23:25:33 -0400

    use indexed vim.cmd for colorscheme

--- neovim/.config/nvim/after/plugin/theme.lua
+++ neovim/.config/nvim/after/plugin/theme.lua
@@ -1,2 +1,2 @@
 vim.opt.background = "dark" -- vim.g.colors_name = 'ayu'
-vim.cmd("colorscheme ayu-mirage") -- Put your favorite colorscheme here
+vim.cmd.colorscheme("ayu-mirage") -- Put your favorite colorscheme here
