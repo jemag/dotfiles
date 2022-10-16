464cadd7c7dfc08a9f91274b550d6955c8be2c8a
jemag <alexandre.bd@tutanota.com>
2022-10-16 18:46:57 -0400

    update sumneko to use neodev (luadev) new setup

--- neovim/.config/nvim/lua/lsp/configs/sumneko_lua.lua
+++ neovim/.config/nvim/lua/lsp/configs/sumneko_lua.lua
@@ -1,28 +1,45 @@
-return require("lua-dev").setup({
+require("neodev").setup({
   library = {
-    vimruntime = true,
-    types = true,
-    plugins = false,
+    enabled = true, -- when not enabled, neodev will not change any settings to the LSP server
+    -- these settings will be used for your Neovim config directory
+    runtime = true, -- runtime path
+    types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+    plugins = false, -- installed opt or start plugins in packpath
+    -- you can also specify the list of plugins to make available as a workspace library
+    -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
   },
-  lspconfig = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          enable = true,
-          disable = {
-            "different-requires",
-          },
-          globals = {
-            "vim", "describe", "it", "before_each", "after_each", "teardown", "pending", "clear"
-          }
+  setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
+  -- for your Neovim config directory, the config.library settings will be used as is
+  -- for plugin directories (root_dirs having a /lua directory), config.library.plugins will be disabled
+  -- for any other directory, config.library.enabled will be set to false
+  override = function(root_dir, options) end,
+})
+
+return {
+  settings = {
+    Lua = {
+      diagnostics = {
+        enable = true,
+        disable = {
+          "different-requires",
+        },
+        globals = {
+          "vim",
+          "describe",
+          "it",
+          "before_each",
+          "after_each",
+          "teardown",
+          "pending",
+          "clear",
         },
       },
     },
-    capabilities = require("lsp.handlers").capabilities,
-    on_attach = require("lsp.handlers").on_attach,
-    flags = {
-      debounce_text_changes = 150,
-      allow_incremental_sync = true,
-    },
   },
-})
+  capabilities = require("lsp.handlers").capabilities,
+  on_attach = require("lsp.handlers").on_attach,
+  flags = {
+    debounce_text_changes = 150,
+    allow_incremental_sync = true,
+  },
+}
