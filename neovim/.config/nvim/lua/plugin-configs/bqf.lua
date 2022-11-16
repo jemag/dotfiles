1f1889bf7c512e3e43c05248187451c227f3a136
jemag <alexandre.bd@tutanota.com>
2022-11-15 23:43:23 -0500

    add bqf keybinds

--- neovim/.config/nvim/lua/plugin-configs/bqf.lua
+++ neovim/.config/nvim/lua/plugin-configs/bqf.lua
@@ -7,4 +7,39 @@ require("bqf").setup({
       extra_opts = { "--bind", "ctrl-e:toggle-all" },
     },
   },
+  func_map = {
+    open = "<cr>",
+    openc = "o",
+    drop = "O",
+    -- tabdrop     =
+    tab = "t",
+    tabb = "T",
+    tabc = "<C-t>",
+    split = "<C-x>",
+    vsplit = "<C-v>",
+    prevfile = "<C-p>",
+    nextfile = "<C-n>",
+    prevhist = "<",
+    nexthist = ">",
+    lastleave = [['"]],
+    stoggleup = "<S-Tab>",
+    stoggledown = "<Tab>",
+    stogglevm = "<Tab>",
+    stogglebuf = "'<Tab>",
+    sclear = "z<Tab>",
+    pscrollup = "<C-u>",
+    pscrolldown = "<C-d>",
+    pscrollorig = "zo",
+    -- toggle fullscreen preview
+    ptogglemode = "zp",
+    -- toggle preview for item
+    ptoggleitem = "p",
+    -- toggle auto preview
+    ptoggleauto = "P",
+    -- create new list for signed items
+    filter = "zn",
+    -- create new list for non-signed items
+    filterr = "zN",
+    fzffilter = "zf",
+  },
 })
