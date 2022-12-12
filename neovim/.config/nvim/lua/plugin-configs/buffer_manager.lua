91814ac550f76c9d255e132618468e0b19d603b1
Alexandre Desjardins <alexandre.bd@tutanota.com>
2022-12-12 16:56:50 -0500

    switch buffer_manager keymaps autocmd to lua

--- neovim/.config/nvim/lua/plugin-configs/buffer_manager.lua
+++ neovim/.config/nvim/lua/plugin-configs/buffer_manager.lua
@@ -13,7 +13,24 @@ require("buffer_manager").setup({
 vim.keymap.set("n", "<leader>bm", function()
   require("buffer_manager.ui").toggle_quick_menu()
 end)
-vim.api.nvim_command([[
-autocmd FileType buffer_manager vnoremap J :m '>+1<CR>gv=gv
-autocmd FileType buffer_manager vnoremap K :m '<-2<CR>gv=gv
-]])
+
+vim.api.nvim_create_augroup("buffer_manager", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "buffer_manager",
+  callback = function()
+    vim.keymap.set("n", "J", "<cmd>:m +1<CR>gv=gv", { buffer = true })
+    vim.keymap.set("n", "K", "<cmd>:m -2<CR>gv=gv", { buffer = true })
+    -- vim.keymap.set("n", "<C-v>", function()
+    --   local picked_window_id = require("window-picker").pick_window()
+    --   vim.api.nvim_set_current_win(picked_window_id)
+    --   vim.cmd("vsplit")
+    -- end, { buffer = true })
+    -- vim.keymap.set("n", "<C-x>", function()
+    --   local picked_window_id = require("window-picker").pick_window()
+    --   vim.api.nvim_set_current_win(picked_window_id)
+    --   vim.cmd("split")
+    -- end, { buffer = true })
+  end,
+  group = "buffer_manager",
+  desc = "Set buffer_manager keybinds",
+})
