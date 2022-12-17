8bf4c357a9bffb4ba6c06b095998ac9fd3b567fa
jemag <alexandre.bd@tutanota.com>
2022-12-16 22:33:10 -0500

    use mason for java, add mason-tool-installer for other misc dependencies

--- neovim/.config/nvim/after/ftplugin/go.lua
+++ neovim/.config/nvim/after/ftplugin/go.lua
@@ -1,3 +1,14 @@
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.tabstop = 2
+-- Run gofmt on save
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').gofmt()
+  end,
+  group = format_sync_grp,
+})
+
