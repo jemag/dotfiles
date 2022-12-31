16170f9c6bf143f543f3e326283faac1b25ec701
jemag <alexandre.bd@tutanota.com>
2022-12-31 11:11:10 -0500

    disable vim regex highlight explicitly

--- neovim/.config/nvim/lua/plugin-configs/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugin-configs/nvim-treesitter.lua
@@ -4,6 +4,7 @@ require("nvim-treesitter.configs").setup({
   },
   highlight = {
     enable = true, -- false will disable the whole extension
+    additional_vim_regex_highlighting = false,
   },
   autopairs = { enable = true },
   incremental_selection = {
