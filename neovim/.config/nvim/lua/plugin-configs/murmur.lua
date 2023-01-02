cf3d74435990ba010c08be04a38f10aff5ca446a
jemag <alexandre.bd@tutanota.com>
2023-01-02 12:05:44 -0500

    streamline murmur's config

--- neovim/.config/nvim/lua/plugin-configs/murmur.lua
+++ neovim/.config/nvim/lua/plugin-configs/murmur.lua
@@ -1,15 +1,4 @@
 require("murmur").setup({
-  -- cursor_rgb = 'purple', -- default to '#393939'
   max_len = 80, -- maximum word-length to highlight
-  -- min_len = 3,
-  -- disable_on_lines = 2000, -- to prevent lagging on large files. Default to 2000 lines.
   exclude_filetypes = {},
-  callbacks = {
-    -- to trigger the close_events of vim.diagnostic.open_float.
-    function()
-      -- Close floating diag. and make it triggerable again.
-      vim.cmd("doautocmd InsertEnter")
-      vim.w.diag_shown = false
-    end,
-  },
 })
