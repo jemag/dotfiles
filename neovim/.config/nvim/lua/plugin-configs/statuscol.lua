a82c39395a934b29e2496a919bee1f345202f0b7
jemag <alexandre.bd@tutanota.com>
2023-01-21 23:12:37 -0500

    swich statuscol sign order, update plugins

--- neovim/.config/nvim/lua/plugin-configs/statuscol.lua
+++ neovim/.config/nvim/lua/plugin-configs/statuscol.lua
@@ -1,7 +1,7 @@
 require("statuscol").setup({
   setopt = true,
   relculright = true,
-  order = "NSFs"
+  order = "SNFs"
 })
 -- vim.o.statuscolumn = "%@v:lua.ScFa@%C%T%@v:lua.ScSa@%s%T%@v:lua.ScLa@%{%v:lua.ScLn()%}%{v:lua.ScSp()}%T"
 -- statuscolumn=%@v:lua.ScFa@%C%T%@v:lua.ScSa@%s%T%@v:lua.ScLa@%{%v:lua.ScLn()%}%{v:lua.ScSp()}%T
