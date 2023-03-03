edb158dd54c9a7c95a72421bb4361eb9c36dc428
jemag <alexandre.bd@tutanota.com>
2023-03-02 22:58:51 -0500

    improve barbar appearance

--- neovim/.config/nvim/lua/plugin-configs/barbar.lua
+++ neovim/.config/nvim/lua/plugin-configs/barbar.lua
@@ -1,49 +1,17 @@
---[[ let bufferline = {}
-let bufferline.icons = "both"
-" Enable/disable animations
-let bufferline.animation = true
-" Enable/disable close button
-let bufferline.closable = true
-
-" Enables/disable clickable tabs
-"  - left-click: go to buffer
-"  - middle-click: delete buffer
-let bufferline.clickable = true
-
-" If set, the letters for each buffer in buffer-pick mode will be
-" assigned based on their name. Otherwise or in case all letters are
-" already assigned, the behavior is to assign letters in order of
-" usability (see order below)
-let bufferline.semantic_letters = true
-
-" New buffer letters are assigned in this order. This order is
-" optimal for the qwerty keyboard layout but might need adjustement
-" for other layouts.
-let bufferline.letters =
-  \ 'asdfjkl;ghnmxcbziowerutyqpASDFJKLGHNMXCBZIOWERUTYQP'
-
-" Sets the maximum padding width with which to surround each tab
-let bufferline.maximum_padding = 4 ]]
-vim.cmd([[autocmd ColorScheme * highlight! BufferCurrent guifg=black guibg=#77A8D9]])
-vim.cmd([[autocmd ColorScheme * highlight! BufferCurrentIndex guifg=black guibg=#77A8D9]])
-vim.cmd([[autocmd ColorScheme * highlight! BufferCurrentSign guifg=#E06C75 guibg=#77A8D9]])
-vim.cmd([[autocmd ColorScheme * highlight! BufferCurrentMod guifg=#E06C75 guibg=#77A8D9]])
-vim.cmd([[autocmd ColorScheme * highlight! BufferVisible guifg=black guibg=#626A73]])
-vim.cmd([[autocmd ColorScheme * highlight! BufferVisibleIndex guifg=black guibg=#626A73]])
-vim.cmd([[autocmd ColorScheme * highlight! BufferVisibleSign guifg=#626A73 guibg=#626A73]])
-require'bufferline'.setup {
+require("bufferline").setup({
   animation = false,
-  auto_hide = false,
+  auto_hide = true,
   tabpages = true,
   closable = true,
   clickable = true,
-  exclude_ft = {'qf'},
+  exclude_ft = { "qf" },
   icons = "both",
   icon_separator_active = "▊ ",
+  icon_separator_inactive = "",
   maximum_padding = 4,
   maximum_length = 40,
   semantic_letters = true,
-}
+})
 vim.api.nvim_set_keymap("n", "H", "<cmd>BufferPrevious<cr>", { noremap = true, silent = true, desc = "Previous buffer" })
 vim.api.nvim_set_keymap("n", "L", "<cmd>BufferNext<cr>", { noremap = true, silent = true, desc = "Next buffer" })
 vim.keymap.set({ "n", "x" }, "<leader>1", "<cmd>BufferGoto 1<cr>", { desc = "Buf 1" })
