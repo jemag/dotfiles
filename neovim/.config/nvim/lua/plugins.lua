e921d7d676376d2da31dc0b1b0c04fef032db7d8
Alexandre Desjardins <alexandre.bd@tutanota.com>
2023-03-17 15:58:42 -0400

    fix lazy auto install

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -5,12 +5,12 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "--single-branch",
-    "git@github.com:folke/lazy.nvim.git",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { "tyru/capture.vim" },
