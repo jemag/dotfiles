116f2b220fdf6ebf0d1d83694ba88e6fe5818c0b
jemag <alexandre.bd@tutanota.com>
2023-04-05 10:23:29 -0400

    disable mini.cursorword in terminal

--- neovim/.config/nvim/lua/plugin-configs/cursorword.lua
+++ neovim/.config/nvim/lua/plugin-configs/cursorword.lua
@@ -8,4 +8,11 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
   command = "hi! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE",
   desc = "Set MiniCursorword highlight",
 })
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+  pattern = "*",
+  callback = function()
+    vim.b.minicursorword_disable = true
+  end,
+  desc = "Disable cursorword in terminal",
+})
 require("mini.cursorword").setup({})
