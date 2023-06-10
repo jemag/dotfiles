8d2c6bdad8f7d94e662cf655d5dcbdfdf91b67aa
jemag <alexandre.bd@tutanota.com>
2023-06-09 22:43:57 -0400

    detach yamlls in helm files

--- neovim/.config/nvim/after/ftplugin/helm.lua
+++ neovim/.config/nvim/after/ftplugin/helm.lua
@@ -1,2 +1,6 @@
-vim.diagnostic.disable(0)
-vim.diagnostic.reset(nil, 0)
+local clients = vim.lsp.get_active_clients()
+for client_id, client in pairs(clients) do
+  if client.name == "yamlls" then
+    vim.lsp.buf_detach_client(0, client_id)
+  end
+end
