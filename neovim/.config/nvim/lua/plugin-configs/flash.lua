dce4176bf579fc66beaceee1590b00c1caac38c5
Alexandre Desjardins <alexandre.bd@tutanota.com>
2023-06-29 15:47:35 -0400

    update plugins, update flash config

--- neovim/.config/nvim/lua/plugin-configs/flash.lua
+++ neovim/.config/nvim/lua/plugin-configs/flash.lua
@@ -1,132 +1,126 @@
 local flash = require("flash")
 flash.setup({
-  -- labels = "abcdefghijklmnopqrstuvwxyz",
-  labels = "asdfghjklqwertyuiopzxcvbnm",
-  search = {
-    -- search/jump in all windows
-    multi_window = false,
-    -- search direction
-    forward = true,
-    -- when `false`, find only matches in the given direction
-    wrap = true,
-    ---@type Flash.Pattern.Mode
-    -- Each mode will take ignorecase and smartcase into account.
-    -- * exact: exact match
-    -- * search: regular search
-    -- * fuzzy: fuzzy search
-    -- * fun(str): custom function that returns a pattern
-    --   For example, to only match at the beginning of a word:
-    --   mode = function(str)
-    --     return "\\<" .. str
-    --   end,
-    mode = "exact",
-    -- behave like `incsearch`
-    incremental = false,
-    filetype_exclude = { "notify", "noice", "cmp_menu", "flash_prompt" },
-    trigger = "",
-  },
-  jump = {
-    -- save location in the jumplist
-    jumplist = true,
-    -- jump position
-    pos = "start", ---@type "start" | "end" | "range"
-    -- add pattern to search history
-    history = false,
-    -- add pattern to search register
-    register = false,
-    -- clear highlight after jump
-    nohlsearch = false,
-    -- automatically jump when there is only one match
-    autojump = false,
-  },
-  highlight = {
-    label = {
-      -- allow uppercase labels
-      uppercase = true,
-      -- add a label for the first match in the current window.
-      -- you can always jump to the first match with `<CR>`
-      current = true,
-      -- show the label after the match
-      after = false, ---@type boolean|number[]
-      -- show the label before the match
-      before = true, ---@type boolean|number[]
-      -- position of the label extmark
-      style = "inline", ---@type "eol" | "overlay" | "right_align" | "inline"
-      -- flash tries to re-use labels that were already assigned to a position,
-      -- when typing more characters. By default only lower-case labels are re-used.
-      reuse = "lowercase", ---@type "lowercase" | "all"
-    },
-    -- show a backdrop with hl FlashBackdrop
-    backdrop = true,
-    -- Highlight the search matches
-    matches = true,
-    -- extmark priority
-    priority = 5000,
-    groups = {
-      match = "FlashMatch",
-      current = "FlashCurrent",
-      backdrop = "FlashBackdrop",
-      label = "FlashLabel",
-    },
-  },
-  -- action to perform when picking a label.
-  -- defaults to the jumping logic depending on the mode.
-  ---@type fun(match:Flash.Match, state:Flash.State)|nil
-  action = nil,
-  -- initial pattern to use when opening flash
-  pattern = "",
-  -- You can override the default options for a specific mode.
-  -- Use it with `require("flash").jump({mode = "forward"})`
-  ---@type table<string, Flash.Config>
-  modes = {
-    -- options used when flash is activated through
-    -- a regular search with `/` or `?`
-    search = {
-      enabled = true, -- enable flash for search
-      highlight = { backdrop = true },
-      jump = { history = true, register = true, nohlsearch = true },
-      search = {
-        -- `forward` will be automatically set to the search direction
-        -- `mode` is always set to `search`
-        -- `incremental` is set to `true` when `incsearch` is enabled
-      },
-    },
-    -- options used when flash is activated through
-    -- `f`, `F`, `t`, `T`, `;` and `,` motions
-    char = {
-      enabled = true,
-      search = { wrap = false },
-      highlight = { backdrop = false },
-      jump = { register = false },
-    },
-    -- options used for treesitter selections
-    -- `require("flash").treesitter()`
-    treesitter = {
-      labels = "abcdefghijklmnopqrstuvwxyz",
-      jump = { pos = "range" },
-      highlight = {
-        label = { before = true, after = true, style = "inline" },
-        backdrop = false,
-        matches = false,
-      },
-    },
-  },
+	-- labels = "abcdefghijklmnopqrstuvwxyz",
+	labels = "asdfghjklqwertyuiopzxcvbnm",
+	search = {
+		-- search/jump in all windows
+		multi_window = false,
+		-- search direction
+		forward = true,
+		-- when `false`, find only matches in the given direction
+		wrap = true,
+		---@type Flash.Pattern.Mode
+		-- Each mode will take ignorecase and smartcase into account.
+		-- * exact: exact match
+		-- * search: regular search
+		-- * fuzzy: fuzzy search
+		-- * fun(str): custom function that returns a pattern
+		--   For example, to only match at the beginning of a word:
+		--   mode = function(str)
+		--     return "\\<" .. str
+		--   end,
+		mode = "exact",
+		-- behave like `incsearch`
+		incremental = false,
+		filetype_exclude = { "notify", "noice", "cmp_menu", "flash_prompt" },
+		trigger = ";",
+	},
+	jump = {
+		-- save location in the jumplist
+		jumplist = true,
+		-- jump position
+		pos = "start", ---@type "start" | "end" | "range"
+		-- add pattern to search history
+		history = false,
+		-- add pattern to search register
+		register = true,
+		-- clear highlight after jump
+		nohlsearch = false,
+		-- automatically jump when there is only one match
+		autojump = false,
+	},
+	label = {
+		-- allow uppercase labels
+		uppercase = true,
+		-- add a label for the first match in the current window.
+		-- you can always jump to the first match with `<CR>`
+		current = true,
+		-- show the label after the match
+		after = false, ---@type boolean|number[]
+		-- show the label before the match
+		before = true, ---@type boolean|number[]
+		-- position of the label extmark
+		style = "inline", ---@type "eol" | "overlay" | "right_align" | "inline"
+		-- flash tries to re-use labels that were already assigned to a position,
+		-- when typing more characters. By default only lower-case labels are re-used.
+		reuse = "lowercase", ---@type "lowercase" | "all"
+	},
+	-- show a backdrop with hl FlashBackdrop
+	backdrop = true,
+	-- Highlight the search matches
+	matches = true,
+	-- extmark priority
+	priority = 5000,
+	groups = {
+		match = "FlashMatch",
+		current = "FlashCurrent",
+		backdrop = "FlashBackdrop",
+		label = "FlashLabel",
+	},
+	-- action to perform when picking a label.
+	-- defaults to the jumping logic depending on the mode.
+	---@type fun(match:Flash.Match, state:Flash.State)|nil
+	action = nil,
+	-- initial pattern to use when opening flash
+	pattern = "",
+	-- You can override the default options for a specific mode.
+	-- Use it with `require("flash").jump({mode = "forward"})`
+	---@type table<string, Flash.Config>
+	modes = {
+		-- options used when flash is activated through
+		-- a regular search with `/` or `?`
+		search = {
+			enabled = true, -- enable flash for search
+			highlight = { backdrop = true },
+			jump = { history = true, register = true, nohlsearch = true },
+			search = {
+				-- `forward` will be automatically set to the search direction
+				-- `mode` is always set to `search`
+				-- `incremental` is set to `true` when `incsearch` is enabled
+			},
+		},
+		-- options used when flash is activated through
+		-- `f`, `F`, `t`, `T`, `;` and `,` motions
+		char = {
+			enabled = true,
+			search = { wrap = false },
+			highlight = { backdrop = false },
+			jump = { register = false },
+		},
+		-- options used for treesitter selections
+		-- `require("flash").treesitter()`
+		treesitter = {
+			labels = "abcdefghijklmnopqrstuvwxyz",
+			jump = { pos = "range" },
+			highlight = {
+				label = { before = true, after = true, style = "inline" },
+				backdrop = false,
+				matches = false,
+			},
+		},
+	},
 })
--- vim.keymap.set({ "n", "x", "o" }, "/", function()
---   flash.jump({
---     search = { forward = true, wrap = true, multi_window = false },
---   })
--- end, { desc = "flash forward" })
--- vim.keymap.set({ "n", "x", "o" }, "?", function()
---   flash.jump({
---     search = { forward = false, wrap = true, multi_window = false },
---   })
--- end, { desc = "flash forward" })
+vim.keymap.set({ "n", "x", "o" }, "<C-q>", function()
+	flash.jump({
+		search = { forward = true, wrap = true, mode = "search", multi_window = false, trigger = ";", incremental = false },
+    jump = { history = true, register = true, nohlsearch = true}
+	})
+end, { desc = "Flash jump" })
 
 vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  callback = function()
-    -- vim.api.nvim_set_hl(0, "FlashLabel", { link = "IncSearch" })
-    vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#ff00ff", bold = true, underline = false })
-  end,
+	pattern = "*",
+	callback = function()
+		-- vim.api.nvim_set_hl(0, "FlashLabel", { link = "IncSearch" })
+		vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#ff00ff", bold = true, underline = false })
+	end,
 })
