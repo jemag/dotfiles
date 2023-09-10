4835b2c240777abddea292090f91228e54a36d46
jemag <alexandre.bd@tutanota.com>
2023-09-10 19:07:29 -0400

    add toggle trigger to clue

--- neovim/.config/nvim/lua/plugin-configs/clue.lua
+++ neovim/.config/nvim/lua/plugin-configs/clue.lua
@@ -6,6 +6,8 @@ miniclue.setup({
     { mode = "x", keys = "<Leader>" },
     { mode = "n", keys = "<localleader>" },
     { mode = "x", keys = "<localleader>" },
+    { mode = "n", keys = "\\" },
+    { mode = "x", keys = "\\" },
     { mode = "n", keys = "[" },
     { mode = "x", keys = "[" },
     { mode = "n", keys = "<cr>" },
