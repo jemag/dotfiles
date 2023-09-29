36eefaf21f83e19d147fd47d603319640a640867
jemag <alexandre.bd@tutanota.com>
2023-09-28 21:13:18 -0400

    update indent blankline to v3

--- neovim/.config/nvim/lua/plugin-configs/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugin-configs/indent-blankline.lua
@@ -1,28 +1,28 @@
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  command = "highlight! IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine",
-})
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  command = "highlight! IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine",
-})
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  command = "highlight! IndentBlanklineIndent3 guifg=#98C379 gui=nocombine",
-})
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  command = "highlight! IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine",
-})
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  command = "highlight! IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine",
-})
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  command = "highlight! IndentBlanklineIndent6 guifg=#C678DD gui=nocombine",
-})
-require("indent_blankline").setup({
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+require("ibl").setup({
+  indent = { highlight = highlight },
   enabled = false,
   char_highlight_list = {
     "IndentBlanklineIndent1",
@@ -32,20 +32,20 @@ require("indent_blankline").setup({
     "IndentBlanklineIndent5",
     "IndentBlanklineIndent6",
   },
-  filetype_exclude = {
-    "help",
-    "terminal",
-    "dashboard",
-    "nofile",
-    "packer",
-    "lspinfo",
-    "TelescopePrompt",
-    "TelescopeResults",
+  exclude = {
+    filetypes = {
+      "help",
+      "terminal",
+      "dashboard",
+      "nofile",
+      "packer",
+      "lspinfo",
+      "TelescopePrompt",
+      "TelescopeResults",
+    },
+  },
+  scope = {
+    enabled = false,
   },
 })
-vim.api.nvim_set_keymap(
-  "n",
-  "<localleader>ti",
-  "<cmd>IndentBlanklineToggle<CR>",
-  { noremap = true, silent = true, desc = "Toggle indentblankline" }
-)
+vim.api.nvim_set_keymap("n", "<localleader>ti", "<cmd>IBLToggle<CR>", { noremap = true, silent = true, desc = "Toggle indentblankline" })
