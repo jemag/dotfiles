765d55168aded6cc7436e30d04d9b2f72d6612c8
jemag <alexandre.bd@tutanota.com>
2023-12-30 10:43:13 -0500

    use main neo-tree which includes render scroll fix

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -180,7 +180,7 @@ require("lazy").setup({
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    -- branch = "v3.x",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       { "nvim-tree/nvim-web-devicons" }, -- not strictly required, but recommended
