14ad09217d4f5cc72b0547969fcfec73f175a6a4
jemag <alexandre.bd@tutanota.com>
2024-01-13 00:16:50 -0500

    set neo-tree branch to main

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -180,7 +180,7 @@ require("lazy").setup({
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    -- branch = "v3.x",
+    branch = "main",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       { "nvim-tree/nvim-web-devicons" }, -- not strictly required, but recommended
