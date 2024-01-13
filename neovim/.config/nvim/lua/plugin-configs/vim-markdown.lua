5489a1e6a3e22e68f5399fcd508b3a22090a9376
jemag <alexandre.bd@tutanota.com>
2024-01-13 00:14:27 -0500

    disable defualt vim_markdown keybinds

--- neovim/.config/nvim/lua/plugin-configs/vim-markdown.lua
+++ neovim/.config/nvim/lua/plugin-configs/vim-markdown.lua
@@ -3,3 +3,4 @@ vim.g.vim_markdown_conceal = 1
 vim.g.vim_markdown_conceal_code_blocks = 1
 vim.g.vim_markdown_new_list_item_indent = 0
 vim.g.vim_markdown_auto_insert_bullets = 0
+vim.g.vim_markdown_no_default_key_mappings = 1
