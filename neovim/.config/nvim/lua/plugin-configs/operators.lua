76a62c6eb009f1c4a08ceec21053b692ba00ad70
jemag <alexandre.bd@tutanota.com>
2024-01-13 00:16:31 -0500

    fix operators keybind

--- neovim/.config/nvim/lua/plugin-configs/operators.lua
+++ neovim/.config/nvim/lua/plugin-configs/operators.lua
@@ -26,7 +26,7 @@ require("mini.operators").setup({
 })
 vim.keymap.set({ "n", "x" }, "<cr>s", '"+s', { remap = true, desc = "Substitute with +reg" })
 vim.keymap.set({ "n" }, "gX", "gx$", { remap = true, desc = "Exchange until end of line" })
-vim.keymap.set({ "n" }, "gm", "gm$", { remap = true, desc = "Multiply until end of line" })
+vim.keymap.set({ "n" }, "gM", "gm$", { remap = true, desc = "Multiply until end of line" })
 vim.keymap.set({ "n" }, "S", "s$", { remap = true, desc = "Substitute until end of line" })
 vim.keymap.set({ "n" }, "gS", "gs$", { remap = true, desc = "Sort until end of line" })
 vim.keymap.set("n", "<cr>ss", '"+ss', {
