7c9ba666b13f174c0780b0f22b2cccd570a673d1
jemag <alexandre.bd@tutanota.com>
2024-02-06 22:21:16 -0500

    use save and remove mode wth arrow.nvim

--- neovim/.config/nvim/lua/plugin-configs/arrow.lua
+++ neovim/.config/nvim/lua/plugin-configs/arrow.lua
@@ -2,6 +2,7 @@ require("arrow").setup({
   show_icons = true,
   always_show_path = false,
   separate_by_branch = false, -- Bookmarks will be separated by git branch
+  separate_save_and_remove = true,
   mappings = {
     edit = "e",
     delete_mode = "d",
@@ -10,6 +11,7 @@ require("arrow").setup({
     open_vertical = "v",
     open_horizontal = "x",
     quit = "q",
+    remove = "r"
   },
   leader_key = "<c-s>",
   global_bookmarks = false, -- if true, arrow will save files globally (ignores separate_by_branch)
