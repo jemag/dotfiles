0cb394796b54f7c87eacce24c403963354cdc616
alexandre desjardins <alexandre.bd@tutanota.com>
2024-02-18 22:10:50 -0500

    update nvim plugins

--- neovim/.config/nvim/lua/plugin-configs/obsidian.lua
+++ neovim/.config/nvim/lua/plugin-configs/obsidian.lua
@@ -8,14 +8,6 @@ require("obsidian").setup({
     nvim_cmp = true,
     -- Trigger completion at 2 chars
     min_chars = 2,
-    -- Where to put new notes created from completion. Valid options are
-    --  * "current_dir" - put new notes in same directory as the current buffer.
-    --  * "notes_subdir" - put new notes in the default notes subdirectory.
-    new_notes_location = "current_dir",
-
-    -- Whether to add the output of the node_id_func to new notes in autocompletion.
-    -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-    prepend_note_id = true,
   },
   finder = "telescope.nvim",
   open_notes_in = "current",
