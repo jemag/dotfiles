58e9fde02a5683895dac9c4244b8688a5d422d14
jemag <alexandre.bd@tutanota.com>
2024-03-09 16:43:18 -0500

    use note path in wiki link

--- neovim/.config/nvim/lua/plugin-configs/obsidian.lua
+++ neovim/.config/nvim/lua/plugin-configs/obsidian.lua
@@ -42,6 +42,7 @@ require("obsidian").setup({
     local path = spec.dir / tostring(spec.title)
     return path:with_suffix(".md")
   end,
+  wiki_link_func = "prepend_note_path",
   note_id_func = function(title)
     -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
     -- In this case a note with the title 'My new note' will be given an ID that looks
