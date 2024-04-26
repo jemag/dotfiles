29ce29bbc2234a36ed06d65945657a4efc36214c
alexandre desjardins <alexandre.bd@tutanota.com>
2024-04-26 18:36:44 -0400

    chore: update telescope-undo keybinds

--- neovim/.config/nvim/lua/plugin-configs/telescope-config.lua
+++ neovim/.config/nvim/lua/plugin-configs/telescope-config.lua
@@ -161,13 +161,13 @@ require("telescope").setup({
       side_by_side = false,
       diff_context_lines = vim.o.scrolloff,
       entry_format = "state #$ID, $STAT, $TIME",
-      -- mappings = {
-      --   i = {
-      --     ["<c-y>"] = require("telescope-undo.actions").yank_additions,
-      --     ["<c-d>"] = require("telescope-undo.actions").yank_deletions,
-      --     ["<cr>"] = require("telescope-undo.actions").restore,
-      --   },
-      -- },
+      mappings = {
+        i = {
+          ["<cr>"] = require("telescope-undo.actions").yank_additions,
+          ["<c-f>"] = require("telescope-undo.actions").yank_deletions,
+          ["<C-r>"] = require("telescope-undo.actions").restore,
+        },
+      },
     },
     media_files = {
       -- filetypes whitelist
