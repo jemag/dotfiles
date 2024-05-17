471de289b912c34fefdcb666f9889680f4f9ef6a
alexandre desjardins <alexandre.bd@tutanota.com>
2024-05-17 12:51:00 -0400

    chore: update luasnip to use pure lua keymaps

--- neovim/.config/nvim/lua/plugin-configs/luasnip.lua
+++ neovim/.config/nvim/lua/plugin-configs/luasnip.lua
@@ -1,8 +1,16 @@
-vim.api.nvim_set_keymap("i", "<C-e>", "<Plug>luasnip-expand-or-jump", { desc = "Expand snip or jump next" })
-vim.api.nvim_set_keymap("s", "<C-e>", "<Plug>luasnip-expand-or-jump", { desc = "Expand snip or jump next" })
-vim.api.nvim_set_keymap("i", "<C-y>", "<Plug>luasnip-jump-prev", { desc = "Jump previous snip item" })
-vim.api.nvim_set_keymap("s", "<C-y>", "<Plug>luasnip-jump-prev", { desc = "Jump previous snip item" })
 local ls = require("luasnip")
+
+vim.keymap.set({ "i", "s" }, "<c-e>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true, desc = "Expand snip or jump next" })
+
+vim.keymap.set({ "i", "s" }, "<c-y>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true, desc = "Jump previous snip item" })
 
 ls.config.setup({
   history = true,
