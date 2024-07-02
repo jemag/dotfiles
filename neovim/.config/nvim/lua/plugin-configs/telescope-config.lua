f40e4cf24036220d86bba02dfea224b98bbb7670
jemag <alexandre.bd@tutanota.com>
2024-07-01 20:31:57 -0400

    chore: fix deprecated telescope-undo option

--- neovim/.config/nvim/lua/plugin-configs/telescope-config.lua
+++ neovim/.config/nvim/lua/plugin-configs/telescope-config.lua
@@ -159,7 +159,9 @@ require("telescope").setup({
       use_delta = true,
       use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
       side_by_side = false,
-      diff_context_lines = vim.o.scrolloff,
+      vim_diff_opts = {
+        ctxlen = vim.o.scrolloff,
+      },
       entry_format = "state #$ID, $STAT, $TIME",
       mappings = {
         i = {
