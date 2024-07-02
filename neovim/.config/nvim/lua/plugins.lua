0c48f609684be5b188ba95eff3fe0dda14be0978
jemag <alexandre.bd@tutanota.com>
2024-07-01 20:10:21 -0400

    chore: switch to yankassassin.nvim

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -92,7 +92,13 @@ require("lazy").setup({
     ft = { "jsonnet" },
   },
   {
-    "svban/YankAssassin.vim",
+    "svban/YankAssassin.nvim",
+    config = function()
+      require("YankAssassin").setup({
+        auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
+        auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
+      })
+    end,
   },
   {
     "danymat/neogen",
