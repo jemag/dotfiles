b7421dd26b9f311a38c2681a1c65021e7a13fc73
alexandre desjardins <alexandre.bd@tutanota.com>
2024-07-12 19:30:20 -0400

    feat: enable concealcursor with markview

--- neovim/.config/nvim/lua/plugin-configs/markview.lua
+++ neovim/.config/nvim/lua/plugin-configs/markview.lua
@@ -1,10 +1,20 @@
 require("markview").setup({
-    buf_ignore = { "nofile" },
-    modes = { "n" },
+  buf_ignore = { "nofile" },
+  modes = { "n" },
 
-    -- Returns the conceallevel to the global value when changing modes
-    restore_conceallevel = true,
-    -- Returns the concealcursor to the global value when changing modes
-    restore_concealcursor = true,
-});
-vim.keymap.set("n", "<localleader>tmv", "<cmd>Markview<CR>", { desc = "Toggle markview"})
+  -- Returns the conceallevel to the global value when changing modes
+  restore_conceallevel = true,
+  -- Returns the concealcursor to the global value when changing modes
+  restore_concealcursor = true,
+  options = {
+    on_enable = {
+      conceallevel = 2,
+      concealcursor = "",
+    },
+    on_disable = {
+      conceallevel = 0,
+      concealcursor = "",
+    },
+  },
+})
+vim.keymap.set("n", "<localleader>tmv", "<cmd>Markview<CR>", { desc = "Toggle markview" })
