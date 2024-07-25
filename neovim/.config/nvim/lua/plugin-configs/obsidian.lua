f9093396047080247888c81eeea237cca5dae85f
jemag <alexandre.bd@tutanota.com>
2024-07-24 23:33:42 -0400

    feat: add keybinds with possibility of visual mode for checking and unchecking checkbox

--- neovim/.config/nvim/lua/plugin-configs/obsidian.lua
+++ neovim/.config/nvim/lua/plugin-configs/obsidian.lua
@@ -27,7 +27,7 @@ require("obsidian").setup({
       opts = { noremap = false, expr = true, buffer = true, desc = "Follow link" },
     },
     -- Toggle check-boxes.
-    ["<leader>oc"] = {
+    ["<leader>oC"] = {
       action = function()
         return require("obsidian").util.toggle_checkbox()
       end,
@@ -61,6 +61,74 @@ require("obsidian").setup({
   end,
 })
 
+local _add_checkbox = function(character, line_num)
+	local line = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, false)[1]
+
+	local checkbox_pattern = "^%s*- %[.] "
+	local checkbox = character or " "
+
+	if not string.match(line, checkbox_pattern) then
+		local unordered_list_pattern = "^(%s*)[-*+] (.*)"
+		if string.match(line, unordered_list_pattern) then
+			line = string.gsub(line, unordered_list_pattern, "%1- [ ] %2")
+		else
+			line = string.gsub(line, "^(%s*)", "%1- [ ] ")
+		end
+	end
+	local capturing_checkbox_pattern = "^(%s*- %[).(%] )"
+	line = string.gsub(line, capturing_checkbox_pattern, "%1" .. checkbox .. "%2")
+
+	-- 0-indexed
+	vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, { line })
+end
+
+local _remove_checkbox = function(line_num)
+	local line = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, false)[1]
+	local checkbox_pattern = "^%s*- %[.]. "
+	local capturing_checkbox_pattern = "^(%s*-) %[.%] (.*)"
+	line = string.gsub(line, capturing_checkbox_pattern, "%1 %2")
+	line = string.gsub(line, checkbox_pattern, "")
+	-- 0-indexed
+	vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, { line })
+end
+
+
+local toggle_checkbox = function(character)
+	-- Check if we are in visual line mode
+	local mode = vim.api.nvim_get_mode().mode
+
+	local toggle_or_remove = function(character, line_num)
+		if (character == nil) then
+			-- Remove checkbox
+			_remove_checkbox(line_num)
+		else
+			-- Add checkbox
+			_add_checkbox(character, line_num)
+		end
+	end
+
+	if mode == 'V' or mode == 'v' then
+		-- Get the range of selected lines
+		vim.cmd([[execute "normal! \<ESC>"]])
+		local vstart = vim.fn.getcharpos("'<")
+		local vend = vim.fn.getcharpos("'>")
+
+		local line_start = vstart[2]
+		local line_end = vend[2]
+
+		-- Iterate over each line in the range and apply the transformation
+		for line_num = line_start, line_end do
+			toggle_or_remove(character, line_num)
+		end
+	else
+		-- Normal mode
+		-- Allow line_num to be optional, defaulting to the current line if not provided (normal mode)
+		local line_num = unpack(vim.api.nvim_win_get_cursor(0))
+		toggle_or_remove(character, line_num)
+	end
+end
+vim.keymap.set({"n", "v"}, "<leader>ox", function() toggle_checkbox("x") end, { desc="Set checkbox", noremap = true })
+vim.keymap.set({"n", "v"}, "<leader>oc", function() toggle_checkbox(" ") end, { desc="Clear checkbox", noremap = true })
 vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<cr>", { desc = "Backlinks" })
 vim.keymap.set("x", "<leader>oe", ":ObsidianExtractNote<cr>", { desc = "Extract to note" })
 vim.keymap.set("n", "<leader>og", "<cmd>ObsidianSearch<cr>", { desc = "Grep in notes" })
