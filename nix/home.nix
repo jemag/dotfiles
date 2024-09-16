3383f60a17f5ae8e5aa061d49e94e12c52dd80f4
alexandre desjardins <alexandre.bd@tutanota.com>
2024-09-15 22:57:40 -0400

    chore: add nix-tree and nix-health

--- nix/home.nix
+++ nix/home.nix
@@ -30,6 +30,8 @@
       nixfmt
       nix-search-cli # search nixpkgs
       manix # search nix options
+      nix-tree  # browse dependency graphs of Nix derivations
+      nix-health # check health of nix installation
 
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
