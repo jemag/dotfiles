ae0faf85b78b002b43b4e211491aa995b53ec6ec
alexandre desjardins <alexandre.bd@tutanota.com>
2024-09-15 22:26:08 -0400

    chore: add nix-search-cli

--- nix/home.nix
+++ nix/home.nix
@@ -28,6 +28,7 @@
       chromium
       rustup
       nixfmt
+      nix-search-cli
 
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
