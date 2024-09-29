dfddccb4cfbf5ac217d6083be675688a34851811
jemag <alexandre.bd@tutanota.com>
2024-09-29 18:42:06 -0400

    chore: remove unneeded highlight-undo keymaps

--- neovim/.config/nvim/lua/plugin-configs/highlight-undo.lua
+++ neovim/.config/nvim/lua/plugin-configs/highlight-undo.lua
@@ -1,11 +1,8 @@
 require("highlight-undo").setup({
   hlgroup = "HighlightUndo",
   duration = 300,
-  keymaps = {
-    { "n", "u", "undo", {} },
-    { "n", "<C-r>", "redo", {} },
-  },
 })
+
 vim.api.nvim_create_autocmd({ "ColorScheme" }, {
   pattern = "*",
   callback = function()
