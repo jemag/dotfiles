3a738759d464c2642ee74a43e3ff12a4279940eb
alexandre desjardins <alexandre.bd@tutanota.com>
2024-10-10 18:58:52 -0400

    chore: add kustomize.nvim run commands

--- neovim/.config/nvim/lua/plugin-configs/kustomize.lua
+++ neovim/.config/nvim/lua/plugin-configs/kustomize.lua
@@ -1,6 +1,20 @@
 require("kustomize").setup({
   enable_key_mappings = false,
   enable_lua_snip = true,
+  run = {
+    validate = {
+      cmd = "kubeconform",
+      args = { "--strict", "--ignore-missing-schemas" },
+    },
+    deprecations = {
+      cmd = "kubent",
+      args = { "-t", "1.26", "-c=false", "--helm3=false", "-l=error", "-e", "-f" },
+    },
+    trivy = {
+        cmd = "trivy",
+        args = { "-q", "config" },
+        },
+  },
 })
 
 vim.keymap.set("n", "<leader>kb", "<cmd>KustomizeBuild<cr>", { noremap = true, desc = "kustomize build" })
