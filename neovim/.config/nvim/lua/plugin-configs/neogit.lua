75e18e443583976260e131af753d1d7e43641f1d
alexandre desjardins <alexandre.bd@tutanota.com>
2024-12-14 13:08:35 -0500

    chore: enable line numbers in neogit and navigating with them

--- neovim/.config/nvim/lua/plugin-configs/neogit.lua
+++ neovim/.config/nvim/lua/plugin-configs/neogit.lua
@@ -5,13 +5,21 @@ neogit.setup({
   -- customize displayed signs
   integrations = {
     diffview = true,
-    telescope = true
+    telescope = true,
   },
+  disable_line_numbers = false,
+  disable_relative_line_numbers = false,
   mappings = {
     popup = {
       ["g?"] = "HelpPopup",
       ["?"] = false,
-    }
-  }
+    },
+    status = {
+      ["1"] = false,
+      ["2"] = false,
+      ["3"] = false,
+      ["4"] = false,
+    },
+  },
 })
 vim.keymap.set("n", "<localleader>tn", "<cmd>Neogit<cr>", { desc = "Neogit" })
