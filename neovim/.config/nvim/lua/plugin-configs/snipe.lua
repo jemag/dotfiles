0b40c507272b6107dcab327271624fa7c3281531
alexandre desjardins <alexandre.bd@tutanota.com>
2024-12-26 15:41:01 -0500

    chore: use snipe for vim.ui.select

--- neovim/.config/nvim/lua/plugin-configs/snipe.lua
+++ neovim/.config/nvim/lua/plugin-configs/snipe.lua
@@ -1,7 +1,15 @@
-require("snipe").setup({
+local snipe = require("snipe")
+snipe.setup({
   ui = {
     position = "center"
   },
 })
 
-vim.keymap.set({ "n", "x" }, "<c-s>", require("snipe").open_buffer_menu, { desc = "Snipe buffer menu" })
+vim.keymap.set({ "n", "x" }, "<c-s>", snipe.open_buffer_menu, { desc = "Snipe buffer menu" })
+snipe.ui_select_menu = require("snipe.menu"):new { position = "center" }
+snipe.ui_select_menu:add_new_buffer_callback(function (m)
+  vim.keymap.set("n", "<esc>", function ()
+    m:close()
+  end, { nowait = true, buffer = m.buf })
+end)
+vim.ui.select = snipe.ui_select;
