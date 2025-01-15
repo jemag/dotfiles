df0e2b8940839a8ae370596db6fc6f6a3f23a1fc
alexandre desjardins <alexandre.bd@tutanota.com>
2025-01-15 17:58:48 -0500

    chore: update bookmark icon for note

--- neovim/.config/nvim/lua/plugin-configs/bookmarks.lua
+++ neovim/.config/nvim/lua/plugin-configs/bookmarks.lua
@@ -6,7 +6,7 @@ require("bookmarks").setup({
     ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
     ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
     ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
-    ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+    ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
   },
   on_attach = function(bufnr)
     local bm = require("bookmarks")
