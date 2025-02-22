ac1cb845ee0fe6bca1de327a49adfb09c4900389
alexandre desjardins <alexandre.bd@tutanota.com>
2025-02-21 20:58:46 -0500

    feat: add csvview.nvim

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -273,7 +273,26 @@ require("lazy").setup({
       require("key-analyzer").setup()
     end,
   },
-  { "stevearc/dressing.nvim" },
+  {
+    "hat0uma/csvview.nvim",
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        -- Text objects for selecting fields
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        -- Excel-like navigation:
+        -- Use <Tab> and <S-Tab> to move horizontally between fields.
+        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+      },
+    },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+    keys = {
+      { "<localleader>tC", "<cmd>CsvViewToggle<cr>", desc = "Toggle CSV View" },
+    }
+  },
   {
     "ryanmsnyder/toggleterm-manager.nvim",
     dependencies = {
