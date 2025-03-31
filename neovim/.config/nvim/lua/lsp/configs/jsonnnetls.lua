88b745144091f01e5d4818d7d772525c0a561967
alexandre desjardins <alexandre.bd@tutanota.com>
2025-03-31 16:53:07 -0400

    chore: disable jsonnet implicit plus by default

--- neovim/.config/nvim/lua/lsp/configs/jsonnnetls.lua
+++ neovim/.config/nvim/lua/lsp/configs/jsonnnetls.lua
@@ -1,3 +1,10 @@
+local stringtoboolean = { ["true"] = true, ["false"] = false }
+
 return {
-  cmd = {"jsonnet-language-server", "-t"},
+  cmd = { "jsonnet-language-server", "-t" },
+  settings = {
+    formatting = {
+      UseImplicitPlus = stringtoboolean[os.getenv("JSONNET_IMPLICIT_PLUS")] or false,
+    },
+  },
 }
