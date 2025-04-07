8149d739dae9afb92227a50afa55793148716ccd
jemag <alexandre.bd@tutanota.com>
2025-04-06 20:19:03 -0400

    chore: rename render-markdown

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -457,8 +457,7 @@ require("lazy").setup({
     end,
   },
   {
-    "MeanderingProgrammer/markdown.nvim",
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     config = function()
       require("plugin-configs.render-markdown")
