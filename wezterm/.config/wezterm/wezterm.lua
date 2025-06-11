52b41f376feebe0d0c2e281717b5c1bb1bd7b15a
alexandre desjardins <alexandre.bd@tutanota.com>
2025-06-10 22:04:19 -0400

    chore: remove window_decorations option in wezterm since causing issues with sway

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -51,7 +51,6 @@ return {
   window_background_opacity = 1.0, -- [1.0] alpha channel value with floating point numbers in the range 0.0 (meaning completely translucent/transparent) through to 1.0 (meaning completely opaque)
   enable_tab_bar = false, -- [true]
   hide_tab_bar_if_only_one_tab = true, -- [false] hide the tab bar when there is only a single tab in the window
-  window_decorations = "RESIZE",
 
   keys = {
     { key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
