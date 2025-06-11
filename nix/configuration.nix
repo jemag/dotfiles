99ece3f73c06ffa59b81bc7c6bd8d72bbc8c5772
alexandre desjardins <alexandre.bd@tutanota.com>
2025-06-10 21:59:48 -0400

    chore: disable ipv6

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -26,6 +26,7 @@
   # Pick only one of the below networking options.
   networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.enableIPv6 = false;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
