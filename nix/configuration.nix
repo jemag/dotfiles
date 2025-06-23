f63b49f9aa0f903cf2534debeb866651e4103bcb
alexandre desjardins <alexandre.bd@tutanota.com>
2025-06-23 19:49:32 -0400

    chore: add bash and killall to nix configuration

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -126,6 +126,8 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    bash
+    killall
     gnumake
     gcc
     autoconf
