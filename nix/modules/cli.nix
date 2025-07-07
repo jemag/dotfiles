dd4402a75f3925f13f2174cc4d7d209a7deaa0e5
alexandre desjardins <alexandre.bd@tutanota.com>
2025-07-07 16:25:57 -0400

    chore: create generic setup for adding custom packages

--- nix/modules/cli.nix
+++ nix/modules/cli.nix
@@ -1,31 +1,7 @@
 { lib, config, pkgs, ... }:
 let
   cfg = config.cli;
-  javaLspScript = pkgs.writeShellScriptBin "javaLspScript" ''
-    set -ev
-    JDTLS_CONFIG_DIR=~/jdtls-config
-    mkdir -p $JDTLS_CONFIG_DIR
-    cp -r ${pkgs.jdt-language-server}/share/java/jdtls/config_linux/* $JDTLS_CONFIG_DIR/
-    chmod -R 755 $JDTLS_CONFIG_DIR
-
-    java \
-    -Declipse.application=org.eclipse.jdt.ls.core.id1 \
-    -Dosgi.bundles.defaultStartLevel=4 \
-    -Declipse.product=org.eclipse.jdt.ls.core.product \
-    -Dlog.protocol=true \
-    -Dlog.level=ALL \
-    -javaagent:"${pkgs.lombok}/share/java/lombok.jar" \
-    -Xms1g \
-    --add-modules=ALL-SYSTEM \
-    --add-opens \
-    java.base/java.util=ALL-UNNAMED \
-    --add-opens \
-    java.base/java.lang=ALL-UNNAMED \
-    -jar ${pkgs.jdt-language-server}/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar \
-    -configuration $JDTLS_CONFIG_DIR \
-    -data \
-    ~/workspace/
-  '';
+  myPkgs = import ../pkgs { inherit pkgs; };
 in {
   options = {
     cli.enable = lib.mkOption {
@@ -146,10 +122,9 @@ in {
         };
       };
 
-      packages = with pkgs; [
+      packages = with pkgs; builtins.attrValues myPkgs ++ [
         aider-chat
         bat
-        javaLspScript
         bash
         btop
         cachix
