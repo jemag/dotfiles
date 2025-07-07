b9e17f4b08268b36655712efb1f168f2cc6ac263
alexandre desjardins <alexandre.bd@tutanota.com>
2025-07-07 15:49:45 -0400

    chore: move javaLspScript to cli.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -22,31 +22,6 @@
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      javaLspScript = pkgs.writeShellScriptBin "javaLspScript" ''
-        set -ev
-        JDTLS_CONFIG_DIR=~/jdtls-config
-        mkdir -p $JDTLS_CONFIG_DIR
-        cp -r ${pkgs.jdt-language-server}/share/java/jdtls/config_linux/* $JDTLS_CONFIG_DIR/
-        chmod -R 755 $JDTLS_CONFIG_DIR
-
-        java \
-        -Declipse.application=org.eclipse.jdt.ls.core.id1 \
-        -Dosgi.bundles.defaultStartLevel=4 \
-        -Declipse.product=org.eclipse.jdt.ls.core.product \
-        -Dlog.protocol=true \
-        -Dlog.level=ALL \
-        -javaagent:"${pkgs.lombok}/share/java/lombok.jar" \
-        -Xms1g \
-        --add-modules=ALL-SYSTEM \
-        --add-opens \
-        java.base/java.util=ALL-UNNAMED \
-        --add-opens \
-        java.base/java.lang=ALL-UNNAMED \
-        -jar ${pkgs.jdt-language-server}/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar \
-        -configuration $JDTLS_CONFIG_DIR \
-        -data \
-        ~/workspace/
-      '';
     in {
       nixosConfigurations = {
         live = lib.nixosSystem {
@@ -79,10 +54,7 @@
         };
         "jemag@nixos" = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [
-            ./hosts/nixos-desktop.nix
-            ({ config, ... }: { home.packages = [ javaLspScript ]; })
-          ];
+          modules = [ ./hosts/nixos-desktop.nix ];
         };
       };
     };
--- nix/modules/cli.nix
+++ nix/modules/cli.nix
@@ -1,5 +1,31 @@
 { lib, config, pkgs, ... }:
-let cfg = config.cli;
+let
+  cfg = config.cli;
+  javaLspScript = pkgs.writeShellScriptBin "javaLspScript" ''
+    set -ev
+    JDTLS_CONFIG_DIR=~/jdtls-config
+    mkdir -p $JDTLS_CONFIG_DIR
+    cp -r ${pkgs.jdt-language-server}/share/java/jdtls/config_linux/* $JDTLS_CONFIG_DIR/
+    chmod -R 755 $JDTLS_CONFIG_DIR
+
+    java \
+    -Declipse.application=org.eclipse.jdt.ls.core.id1 \
+    -Dosgi.bundles.defaultStartLevel=4 \
+    -Declipse.product=org.eclipse.jdt.ls.core.product \
+    -Dlog.protocol=true \
+    -Dlog.level=ALL \
+    -javaagent:"${pkgs.lombok}/share/java/lombok.jar" \
+    -Xms1g \
+    --add-modules=ALL-SYSTEM \
+    --add-opens \
+    java.base/java.util=ALL-UNNAMED \
+    --add-opens \
+    java.base/java.lang=ALL-UNNAMED \
+    -jar ${pkgs.jdt-language-server}/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar \
+    -configuration $JDTLS_CONFIG_DIR \
+    -data \
+    ~/workspace/
+  '';
 in {
   options = {
     cli.enable = lib.mkOption {
@@ -125,6 +151,7 @@ in {
       packages = with pkgs; [
         aider-chat
         bat
+        javaLspScript
         bash
         btop
         cachix
