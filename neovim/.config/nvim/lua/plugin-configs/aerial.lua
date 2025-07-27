84cb4f191872989681c8de4859f9a06da1d550fe
alexandre desjardins <alexandre.bd@tutanota.com>
2025-07-27 12:43:40 -0400

    chore: remove aerial keybindg

--- neovim/.config/nvim/lua/plugin-configs/aerial.lua
+++ neovim/.config/nvim/lua/plugin-configs/aerial.lua
@@ -21,9 +21,6 @@ require("aerial").setup({
     -- Jump forwards/backwards with '{' and '}'
     vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", { desc = "Aerial prev" })
     vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", { desc = "Aerial prev" })
-    -- Jump up the tree with '[[' or ']]'
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", ":lua require('aerial').prev_up()<CR>", { desc = "Aerial prev up" })
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", ":lua require('aerial').next_up()<CR>", { desc = "Aerial next up" })
   end,
 })
 vim.keymap.set("n", "<localleader>ta", "<cmd>AerialToggle!<CR>", { desc = "Toggle aerial" })
