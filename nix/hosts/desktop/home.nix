90148498c164e03c8ecb236baaac1a8c423e7de5
alexandre desjardins <alexandre.bd@tutanota.com>
2025-08-03 23:36:32 -0400

    chore: add gammastep as redhisft alternative with wayland

--- nix/hosts/desktop/home.nix
+++ nix/hosts/desktop/home.nix
@@ -34,7 +34,7 @@
       brave
       mangohud
       qbittorrent
-      protonplus
+      gammastep
       # # Adds the 'hello' command to your environment. It prints a friendly
       # # "Hello, world!" when run.
 
