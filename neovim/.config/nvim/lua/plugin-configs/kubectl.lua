96fe2308de8d331aa18a13a5dab269b9d902b36f
alexandre desjardins <alexandre.bd@tutanota.com>
2025-08-08 17:57:12 -0400

    chore: use ctrl-n and ctrl-p for completion menus in kubectl.nvim

--- neovim/.config/nvim/lua/plugin-configs/kubectl.lua
+++ neovim/.config/nvim/lua/plugin-configs/kubectl.lua
@@ -40,6 +40,8 @@ vim.api.nvim_create_autocmd("FileType", {
     -- Global
     -- k("n", "<localleader>th", "<Plug>(kubectl.toggle_headers) | <Plug>(kubectl.refresh)", opts) -- Toggle headers
     k("n", "<localleader>th", "<Plug>(kubectl.toggle_headers)", opts) -- Toggle headers
+    k("i", "<c-n>", "<Plug>(kubectl.tab)", opts) -- Toggle headers
+    k("i", "<c-p>", "<Plug>(kubectl.shift_tab)", opts) -- Toggle headers
     vim.defer_fn(function()
       for i = 1, 6 do
         pcall(vim.keymap.del, "n", tostring(i), opts)
