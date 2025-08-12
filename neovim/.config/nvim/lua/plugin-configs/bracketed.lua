8806354eb7a43054fe29ad43a4023f22116487a5
alexandre desjardins <alexandre.bd@tutanota.com>
2025-08-12 16:55:13 -0400

    chore: re-enable diagnostics through mini.bracketed

--- neovim/.config/nvim/lua/plugin-configs/bracketed.lua
+++ neovim/.config/nvim/lua/plugin-configs/bracketed.lua
@@ -1,7 +1,6 @@
 require("mini.bracketed").setup({
   -- Disabling in favor of ones from |mini.indentscope|)
   indent = { suffix = "" },
-  diagnostic = { suffix = "" },
   buffer = { suffix = "" },
   file = { suffix = "" },
   treesitter = { suffix = "" },
