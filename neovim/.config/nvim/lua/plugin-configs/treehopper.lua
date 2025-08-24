d31ebf60fe677e0f4ba7c16b4df42f803999f6dd
alexandre desjardins <alexandre.bd@tutanota.com>
2025-08-24 11:40:13 -0400

    chore: remove duplicate keybind

--- neovim/.config/nvim/lua/plugin-configs/treehopper.lua
+++ neovim/.config/nvim/lua/plugin-configs/treehopper.lua
@@ -1,2 +1,1 @@
-vim.api.nvim_set_keymap("o", "m", ":<C-U>lua require('tsht').nodes()<CR>", { noremap = true, silent = true, desc = "Treesitter hints" })
 vim.api.nvim_set_keymap("x", "m", ":lua require('tsht').nodes()<CR>", { noremap = true, silent = true, desc = "Treesitter hints" })
