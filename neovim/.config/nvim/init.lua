070882771eaee6d48e42a28465bb0db60ceb75bf
alexandre desjardins <alexandre.bd@tutanota.com>
2025-08-25 16:15:20 -0400

    chore: clean up unused nvim code

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -33,34 +33,9 @@ vim.opt.shadafile = shadafile
 
 require("settings")
 
-local should_profile = os.getenv("NVIM_PROFILE")
-if should_profile then
-  require("profile").instrument_autocmds()
-  if should_profile:lower():match("^start") then
-    require("profile").start("*")
-  else
-    require("profile").instrument("*")
-  end
-end
-
-local function toggle_profile()
-  local prof = require("profile")
-  if prof.is_recording() then
-    prof.stop()
-    vim.ui.input({ prompt = "Save profile to:", completion = "file", default = "profile.json" }, function(filename)
-      if filename then
-        prof.export(filename)
-        vim.notify(string.format("Wrote %s", filename))
-      end
-    end)
-  else
-    prof.start("*")
-  end
-end
--- vim.keymap.set("", "<f1>", toggle_profile)
 
 -- function to do continuous horizontal scrolling
-vim.cmd([[ 
+vim.cmd([[
 function! HorizontalScrollMode( call_char )
   if &wrap
     return
