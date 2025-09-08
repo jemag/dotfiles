13ead50a785b3566884c2509f1e8d856574ad8a6
alexandre desjardins <alexandre.bd@tutanota.com>
2025-09-08 16:28:08 -0400

    fix: use proper option for jsonnet foldexpr and foldmethod

--- neovim/.config/nvim/after/ftplugin/jsonnet.lua
+++ neovim/.config/nvim/after/ftplugin/jsonnet.lua
@@ -1,4 +1,4 @@
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
-vim.bo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.bo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldmethod = "expr"
