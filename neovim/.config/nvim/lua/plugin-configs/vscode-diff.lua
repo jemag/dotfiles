8563e4d50757e81fb813134fb34a3932db682e52
alexandre desjardins <alexandre.bd@tutanota.com>
2025-11-27 17:12:41 -0500

    feat: configure basic keybinds for vscode-diff.nvim

---
+++ neovim/.config/nvim/lua/plugin-configs/vscode-diff.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>gdv", "<cmd>CodeDiff<cr>", { desc = "CodeDiff" })
+vim.keymap.set("n", "<leader>gdV", ":CodeDiff ", { desc = "CodeDiff custom" })
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -477,6 +477,9 @@ require("lazy").setup({
   {
     "esmuellert/vscode-diff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
+    config = function()
+      require("plugin-configs.vscode-diff")
+    end
   },
   {
     "sindrets/diffview.nvim",
