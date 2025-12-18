40ade867e97e64fc7c3a0e565086fe6164dc90a0
alexandre desjardins <alexandre.bd@tutanota.com>
2025-12-18 10:12:13 -0500

    chore: diff with tree view and relativeline numbers

--- neovim/.config/nvim/lua/plugin-configs/vscode-diff.lua
+++ neovim/.config/nvim/lua/plugin-configs/vscode-diff.lua
@@ -1,2 +1,18 @@
+require("vscode-diff").setup({
+  explorer = {
+    width = 50,
+    view_mode = "tree",
+  },
+})
 vim.keymap.set("n", "<leader>gdv", "<cmd>CodeDiff<cr>", { desc = "CodeDiff" })
 vim.keymap.set("n", "<leader>gdV", ":CodeDiff ", { desc = "CodeDiff custom" })
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Relative line numbers for vscode-diff-explorer",
+  pattern = { "vscode-diff-explorer" },
+  callback = function()
+    vim.schedule(function()
+      vim.wo.relativenumber = true
+    end)
+  end,
+})
