71e55943071f55f3b453bf0f631320b09ff59ace
alexandre desjardins <alexandre.bd@tutanota.com>
2025-12-21 12:28:38 -0500

    chore: change copy json path keybind, copy to both registers

--- neovim/.config/nvim/after/ftplugin/json.lua
+++ neovim/.config/nvim/after/ftplugin/json.lua
@@ -1,5 +1,7 @@
 vim.g.vim_json_syntax_conceal = 0
 
-vim.keymap.set("n", "y<C-p>", function()
-  vim.fn.setreg("+", require("jsonpath").get())
+vim.keymap.set("n", "<localleader>jy", function()
+  local jsonPath = require("jsonpath").get()
+  vim.fn.setreg("+", jsonPath)
+  vim.fn.setreg('"', jsonPath)
 end, { desc = "copy json path", buffer = true })
