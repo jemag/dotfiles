674ce70390fdd6f9bce3af3c6487a1b872411a1a
alexandre desjardins <alexandre.bd@tutanota.com>
2026-01-07 16:39:49 -0500

    chore: switch to opus with code companion

--- neovim/.config/nvim/lua/plugin-configs/codecompanion.lua
+++ neovim/.config/nvim/lua/plugin-configs/codecompanion.lua
@@ -32,7 +32,7 @@ if vim.fn.getenv("WSL_INTEROP") ~= vim.NIL then
             schema = {
               model = {
                 -- default = "claude-sonnet-4",
-                default = "claude-sonnet-4.5",
+                default = "claude-opus-4.5",
                 -- default = "gpt-4.1",
                 -- default = "gemini-2.5-pro",
               },
