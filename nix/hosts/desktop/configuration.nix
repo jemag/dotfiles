f61b492a2841bf6839bdf677789e60754547fbe2
alexandre desjardins <alexandre.bd@tutanota.com>
2026-01-25 11:17:32 -0500

    chore: update nix flake

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -38,7 +38,6 @@
   networking.hostName = "desktop"; # Define your hostname.
   systemd.network.enable = false;
   # Pick only one of the below networking options.
-  networking.wireless.enable = false; # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
