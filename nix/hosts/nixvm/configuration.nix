72b21a94503b83952e3128523b9598b70e9540d4
alexandre desjardins <alexandre.bd@tutanota.com>
2026-03-09 20:29:31 -0400

    chore: use zstd for now for nixvm zswap algo

--- nix/hosts/nixvm/configuration.nix
+++ nix/hosts/nixvm/configuration.nix
@@ -38,7 +38,7 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
     "zswap.enabled=1" # enables zswap
-    "zswap.compressor=lz4" # compression algorithm
+    "zswap.compressor=zstd" # compression algorithm
     "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
     "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
   ];
