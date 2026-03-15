2596bfedd0ca4463121caafb6c6aa3cc0984f16e
alexandre desjardins <alexandre.bd@tutanota.com>
2026-03-15 14:44:20 -0400

    chore: move common base configuration for nixos to base module

--- nix/hosts/nixvm/configuration.nix
+++ nix/hosts/nixvm/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 {
   config,
   lib,
@@ -11,31 +7,13 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../modules/nixos/base.nix
   ];
 
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
-  nix.settings = {
-    trusted-users = [ "jemag" ];
-    auto-optimise-store = true;
-  };
+  networking.hostName = "nixvm"; # Define your hostname.
+  systemd.network.enable = false;
 
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    options = "--delete-older-than +5";
-  };
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
     "preempt=full"
     "zswap.enabled=1" # enables zswap
@@ -43,76 +21,6 @@
     "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
     "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
   ];
-  boot.supportedFilesystems.zfs = lib.mkForce false;
-
-  networking.hostName = "nixvm"; # Define your hostname.
-  systemd.network.enable = false;
-  # Pick only one of the below networking options.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # systemd.user.services.spice-vdagent-client = {
-  #   description = "Spice VDAgent Client";
-  #   wantedBy = [ "graphical-session.target" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.spice-vdagent}/bin/spice-vdagent-client";
-  #     Restart = "on-failure";
-  #     RestartSec = 5;
-  #   };
-  # };
-  # systemd.user.services.spice-vdagent-client.enable = true;
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  services.logind.settings.Login = {
-    IdleAction = "ignore";
-    IdleActionSec = 0;
-  };
-
-  # services.ollama = {
-  #   enable = true;
-  #   package = pkgs.ollama-rocm;
-  #   environmentVariables = {
-  #     OLLAMA_CONTEXT_LENGTH = "32000";
-  #     OLLAMA_KEEP_ALIVE = "24h";
-  #     OLLAMA_NUM_GPU_LAYERS = "999";
-  #     HCC_AMDGPU_TARGET = "gfx1200";
-  #   };
-  # };
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
 
   hardware.bluetooth = {
     enable = true;
@@ -121,31 +29,18 @@
 
   services.blueman.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.jemag = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "docker"
-      "render"
-      "video"
-      "libvirtd"
-    ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [ tree ];
-  };
+  users.users.jemag.extraGroups = [
+    "wheel"
+    "docker"
+    "render"
+    "video"
+    "libvirtd"
+  ];
 
   programs.virt-manager.enable = true;
   virtualisation.libvirtd.enable = true;
 
-  # programs.firefox.enable = true;
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-  programs.steam = {
-    enable = true;
-  };
+  programs.steam.enable = true;
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
@@ -168,39 +63,25 @@
   };
   programs.waybar.enable = true;
 
-  virtualisation.docker = {
-    enable = true;
-  };
+  virtualisation.docker.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    bash
     dnsmasq
     killall
     gnumake
     gcc
     autoconf
     automake
-    bitwarden-cli
     libtool
     mesa
-    git
     mullvad-vpn
     mullvad-browser
     kdePackages.dolphin
-    # rocmPackages.clr
-    # rocmPackages.rocblas
-    # rocmPackages.rocm-smi
-    # rocmPackages.rocminfo
-    # llama-cpp-rocm
     wezterm
     vlc
     wget
   ];
 
-  # Fix for Dolphin lacking file associations
   environment.etc."xdg/menus/applications.menu".source =
     "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 
@@ -210,97 +91,6 @@
   services.displayManager.gdm.enable = true;
   services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
-
-  nixpkgs.config.allowUnfree = true;
-
-  # services.syncthing = {
-  #   enable = true;
-  #   user = "jemag";
-  #   dataDir = "/home/jemag";
-  #   configDir = "/home/jemag/.config/syncthing";
-  #   openDefaultPorts = true;
-  #   settings = {
-  #     devices = {
-  #       "pixel7" = {
-  #         id = "RPTKM3C-B57PYYP-4PLM32G-XQPHDH4-JPMVDQ5-GPJWZLA-3QKKWVT-B2J7QAE";
-  #       };
-  #     };
-  #     folders = {
-  #       "obsidian" = {
-  #         id = "obsidian";
-  #         path = "/home/jemag/H/obsidian";
-  #         devices = [ "pixel7" ];
-  #       };
-  #       # "camera" = {
-  #       #   id = "pixel_7_n5v1-photos";
-  #       #   type = "receiveonly";
-  #       #   path = "/home/jemag/syncthing/camera";
-  #       #   devices = [ "pixel7" ];
-  #       # };
-  #       # "pictures" = {
-  #       #   id = "o2cnd-52gae";
-  #       #   type = "receiveonly";
-  #       #   path = "/home/jemag/syncthing/pictures";
-  #       #   devices = [ "pixel7" ];
-  #       # };
-  #     };
-  #     gui = {
-  #       user = "jemag";
-  #     };
-  #   };
-  # };
-
-  # NOTE: password for the user must be configured using smbpasswd -a jemag
-  # services.samba = {
-  #   enable = true;
-  #   openFirewall = true;
-  #   settings = {
-  #     global = {
-  #       workgroup = "WORKGROUP";
-  #       "invalid users" = [ "root" ];
-  #       "passwd program" = "/run/wrappers/bin/passwd %u";
-  #       security = "user";
-  #       "guest ok" = "no";
-  #     };
-  #     jemag = {
-  #       browseable = "yes";
-  #       comment = "Home directories";
-  #       path = "/home/jemag";
-  #       "read only" = "yes";
-  #       "valid users" = "jemag";
-  #     };
-  #   };
-  # };
-  # services.samba-wsdd = {
-  #   enable = true;
-  #   openFirewall = true;
-  # };
-
-  # services.xserver.videoDrivers = [ "amdgpu" ];
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-  fonts.packages = with pkgs; [
-    fira-code
-    fira
-    jetbrains-mono
-    fira-code-symbols
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   xdg.portal = {
     config = {
@@ -317,35 +107,5 @@
     wlr.enable = true;
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = true;
-  # networking.firewall.allowPing = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
-
 }
---
+++ nix/modules/nixos/base.nix
@@ -0,0 +1,97 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  options.base = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable base NixOS configuration shared across all hosts";
+    };
+  };
+
+  config = lib.mkIf config.base.enable {
+    nix.extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    nix.settings = {
+      trusted-users = [ "jemag" ];
+      auto-optimise-store = true;
+    };
+
+    nix.gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than +5";
+    };
+
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.configurationLimit = 10;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.efiSysMountPoint = "/boot";
+
+    # Use latest kernel.
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.supportedFilesystems.zfs = lib.mkForce false;
+
+    networking.networkmanager.enable = true;
+
+    time.timeZone = "America/New_York";
+
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
+
+    services.logind.settings.Login = {
+      IdleAction = "ignore";
+      IdleActionSec = 0;
+    };
+
+    services.pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+
+    users.users.jemag = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      packages = with pkgs; [ tree ];
+    };
+
+    programs.zsh.enable = true;
+    users.defaultUserShell = pkgs.zsh;
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    fonts.packages = with pkgs; [
+      fira-code
+      fira
+      jetbrains-mono
+      fira-code-symbols
+    ];
+
+    services.openssh.enable = true;
+
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+      vim
+      bash
+      git
+      bitwarden-cli
+    ];
+
+    system.stateVersion = "25.05";
+  };
+}
