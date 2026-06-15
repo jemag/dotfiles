93a6cb4d6de3dfa3fb5fb3d756a1efbc55a54e31
alexandre desjardins <alexandre.bd@tutanota.com>
2026-06-15 10:28:56 -0400

    chore: add videre.nvim for json/yaml/toml exploration

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -358,6 +358,18 @@ require("lazy").setup({
   --   end,
   -- },
   {
+    "Owen-Dechow/videre.nvim",
+    cmd = "Videre",
+    dependencies = {
+      "Owen-Dechow/graph_view_yaml_parser", -- Optional: add YAML support
+      "Owen-Dechow/graph_view_toml_parser", -- Optional: add TOML support
+      "a-usr/xml2lua.nvim", -- Optional | Experimental: add XML support
+    },
+    opts = {
+      box_style = "sharp",
+    },
+  },
+  {
     "nvim-neotest/neotest",
     config = function()
       require("plugin-configs.neotest")
