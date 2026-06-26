87fddc149461a77ea9f812529f6e2905e66c9e4e
alexandre desjardins <alexandre.bd@tutanota.com>
2026-06-26 17:48:18 -0400

    chore: try bufreadpost instead of bufenter for nvim-lint

--- neovim/.config/nvim/lua/plugin-configs/nvim-lint.lua
+++ neovim/.config/nvim/lua/plugin-configs/nvim-lint.lua
@@ -7,7 +7,7 @@ lint.linters_by_ft = {
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
   group = lint_augroup,
   callback = function()
     lint.try_lint()
