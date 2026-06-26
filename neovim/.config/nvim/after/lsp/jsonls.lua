388542c71eda2de6196f0d4b284a5d68c0ac5115
alexandre desjardins <alexandre.bd@tutanota.com>
2026-06-26 18:05:41 -0400

    chore: remove deprecated range_formatting call

--- neovim/.config/nvim/after/lsp/jsonls.lua
+++ neovim/.config/nvim/after/lsp/jsonls.lua
@@ -9,7 +9,7 @@ return {
     commands = {
       Format = {
         function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
+          vim.lsp.buf.format({ range = { start = { 0, 0 }, ["end"] = { vim.fn.line "$", 0 } } })
         end,
       },
     },
