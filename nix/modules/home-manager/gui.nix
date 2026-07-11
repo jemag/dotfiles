bf8859d225dd2ea4a844dcf087649dbe25b7227e
alexandre desjardins <alexandre.bd@tutanota.com>
2026-07-11 14:31:49 -0400

    chore: explicitly enable pointer cursor

--- nix/modules/home-manager/gui.nix
+++ nix/modules/home-manager/gui.nix
@@ -140,6 +140,7 @@ in {
       XDG_THEME_PREFERENCE = "dark";
     };
     home.pointerCursor = {
+      enable = true;
       gtk.enable = true;
       package = pkgs.bibata-cursors;
       name = "Bibata-Original-Classic";
